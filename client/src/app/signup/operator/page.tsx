de1b685fd54126333a477602c21b83c186b5090c
sreyas <sreyassatheesh755@gmail.com>
2024-10-25 23:17:59 +0530

    refactor: Home button position change on small screens in operator signup page

--- client/src/app/signup/operator/page.tsx
+++ client/src/app/signup/operator/page.tsx
@@ -9,7 +9,7 @@ const page = () => {
     <div className="lg:grid-cols-5 xl:min-h-screen w-full grid h-screen">
       <Link
         href="/"
-        className="absolute top-4 right-4 z-10 flex items-center gap-2 py-3 px-3 lg:py-2 lg:px-4 bg text-background rounded-xl text-sm bg-black/40 backdrop-blur-md border border-black/10 shadow-lg"
+        className="absolute top-4 left-4 lg:left-auto lg:right-4 z-10 flex items-center gap-2 py-3 px-3 lg:py-2 lg:px-4 bg text-background rounded-xl text-sm bg-black/40 backdrop-blur-md border border-black/10 shadow-lg"
       >
         <ArrowLeft className="w-4 h-4" strokeWidth={2.5} />
         <span className="hidden lg:block">Back to Home</span>
